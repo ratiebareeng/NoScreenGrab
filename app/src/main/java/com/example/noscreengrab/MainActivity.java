8cc141ffa8881b3ca5085f31bad8022833e07646
ratiebareeng <bareengoc@gmail.com>
2020-07-24 15:02:37 +0200

    This application prevents users from taking screenshot pictures and videos.
    http://developer.android.com/reference/android/view/WindowManager.LayoutParams.html#FLAG_SECURE
    
    FLAG_SECURE: Window flag: treat the content of the window as secure, preventing it from appearing in screenshots or from being viewed on non-secure displays.

--- app/src/main/java/com/example/noscreengrab/MainActivity.java
+++ app/src/main/java/com/example/noscreengrab/MainActivity.java
@@ -10,7 +10,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // INFLATE ACTIVITY LAYOUT
         setContentView(R.layout.activity_main);
+        // THIS FLAG TREATS THE CONTENTS OF THE ACTIVITY LAYOUT AS SECURE AND PREVENTS TAKING SCREENSHOTS
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
     }
 }
